90f1911050d5510c128e8e04964b94eed2af4609
Mathias Pedersen <pedersen.mathias@gmail.com>
2015-09-28 02:18:01 +0200

    Første oppsett av HTML og CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+function showhide()
+ {
+       var div = document.getElementById(&ldquo;nav-main-menu");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
